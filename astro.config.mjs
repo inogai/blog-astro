6f07e084bd1eb61d677f8e66ab221fcf0676b519
inogai <inogai.sign@gmail.com>
2025-10-09 17:12:19 +0800

    chore: configure for gh-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,9 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.inogai.com/',
+  base: '/',
+
   vite: {
     plugins: [tailwindcss()],
   },
