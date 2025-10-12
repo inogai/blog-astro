113907c46db3cca731ed235a1ee1117ca3b7f2e6
inogai <inogai.sign@gmail.com>
2025-10-12 23:46:25 +0800

    fix: use `unist-util-visit` for remarkCjkWrap, making it compatible with callout

--- src/lib/remarkCjkWrap.ts
+++ src/lib/remarkCjkWrap.ts
@@ -1,5 +1,6 @@
 import type { Root } from 'mdast'
 import { all as cjkAll } from 'cjk-regex'
+import { visit } from 'unist-util-visit'
 
 // Known Issues: `“` `”` `‘` `’` can be used in CJK text and latin text, but they are not included in cjk-regex.
 // I use 「」『』 personally so this don't affect me, but if you land here, you might want to append that to your regex
@@ -13,23 +14,19 @@ function processText(text: string): string {
   return text.replaceAll(REGEX, '$1')
 }
 
+/**
+ * Plugin to remove line breaks between CJK characters.
+ *
+ * @returns Transform function.
+ */
 export default function remarkCjkWrap() {
+  /**
+   * @param {Root} tree - The markdown AST.
+   */
   return (tree: Root) => {
-    tree.children = tree.children.map((node) => {
-      if (node.type !== 'paragraph')
-        return node
-
-      return {
-        ...node,
-        children: node.children.map((child) => {
-          if (!('value' in child))
-            return child
-
-          return {
-            ...child,
-            value: processText(child.value),
-          }
-        }),
+    visit(tree, (node) => {
+      if ('value' in node && typeof node.value === 'string') {
+        node.value = processText(node.value)
       }
     })
   }
